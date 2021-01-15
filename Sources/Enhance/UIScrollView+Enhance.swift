4126ed238badd1f4d6f0b6bf5ec609453e0b6c95
Kyle Bashour <kbashour@squareup.com>
2021-01-15 11:07:10 -0800

    Fix whitespace

--- Sources/Enhance/UIScrollView+Enhance.swift
+++ Sources/Enhance/UIScrollView+Enhance.swift
@@ -8,12 +8,12 @@
 import UIKit
 
 extension UIScrollView {
-    
+
     /// Scroll to top using the same animation as tapping the status bar.
     func scrollToTop() {
         let selectorName = ["Possible:", "If", "Top", "To", "_scroll"].reversed().joined()
         let selector = NSSelectorFromString(selectorName)
-				if responds(to: selector) {
+        if responds(to: selector) {
             perform(selector, with: false)
         } else {
             let offset = CGPoint(x: 0, y: -adjustedContentInset.top)
